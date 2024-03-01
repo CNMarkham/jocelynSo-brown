1b84eb4f71f26a1e4fa422b04a55fc5d0b0d2cf7
Griff <markhamonca@codeninjas.com>
2024-03-01 18:01:27 -0500

    done
    blah

---
+++ NinjaDoNotRun/Assets/Scripts/Pickups.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pickups : MonoBehaviour
+{
+    public int score;
+    public Text scoreText;
+
+    public ParticleSystem Pickup;
+    void Start()
+    {
+        Pickup.Stop();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("coin")){
+            score++; //++ adds one to the varible
+            scoreText.text = score.ToString();
+            Destroy(other.gameObject);
+
+            Pickup.Play();
+        }
+    }
+}
--- js  - doNotEscapeJungle/Assets/Scripts/DetectFalsePlatforms.cs
+++ js  - doNotEscapeJungle/Assets/Scripts/DetectFalsePlatforms.cs
@@ -7,8 +7,19 @@ public class DetectFalsePlatforms : MonoBehaviour
 
     public bool hit;
 
+    public LayerMask layermask;
+
     void Update()
     {
-        hit = Physics.Raycast(transform.position, Vector3.forward, );
+        hit = Physics.Raycast(transform.position, Vector3.forward, 10, layermask);
+
+       
+        if (hit)
+        {
+            Debug.LogWarning("Be careful.");
+        } else
+        {
+            Debug.LogWarning("All clear.");
+        }
     }
 }
