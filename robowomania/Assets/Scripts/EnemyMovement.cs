0a4e88bc97bc1d61acc338f41c4348569097586a
Roosham <markhamonca@codeninjas.com>
2024-03-22 18:00:47 -0400

    its done but not done
    :clock530:

--- robowomania/Assets/Scripts/EnemyMovement.cs
+++ robowomania/Assets/Scripts/EnemyMovement.cs
@@ -4,8 +4,42 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
+    public float yForce;
+    public float xForce;
+    public float xDirection;
+    private Rigidbody2D enemyRigidBody;
 
-    public float speed;
+    void Start()
+    {
+        enemyRigidBody = GetComponent<Rigidbody2D>();
+    }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            Vector2 jumpForce = new Vector2(xForce, yForce);
+            enemyRigidBody.AddForce(jumpForce);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (transform.position.x <= -8)
+        {
+
+        }
+
+        if (transform.position.x >= 8)
+        {
+
+        }
+
+
+        float newYPosition = transform.position.y;
+
+
+    }
 }
-   
+
+
