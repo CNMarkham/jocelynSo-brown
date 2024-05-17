6eda4ee9f55bd4cd8506b05e4b1f63de1ea6f86f
boomie_123 <markhamonca@codeninjas.com>
2024-05-17 17:51:05 -0400

    hi
    hi

---
+++ pow pow kachow pt2/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int currentPlayerHealth = 12;
+    public int enemyDamage = 1;
+
+    private Animator playerAnimator;
+    public PlayerExplosionParticles particles;
+
+    void Start()
+    {
+        playerAnimator = GetComponent<Animator>();
+        particles = GetComponent<PlayerExplosionParticles>();
+    }
+
+    public void HurtPlayer()
+    {
+        currentPlayerHealth -= enemyDamage;
+        playerAnimator.SetTrigger("Hit");
+
+        if (currentPlayerHealth <= 0)
+        {
+            particles.Explode();
+            Invoke("ReloadScene", 5);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "HitCollider")
+        {
+            HurtPlayer();
+        }
+    }
+
+    private void ReloadScene()
+    {
+        SceneManager.LoadScene("CyberFu");
+    }
+
+}
